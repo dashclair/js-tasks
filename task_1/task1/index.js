db72592872514ef922e1cddf7f63be90bd7f0cb0
dashclair <darya.gapeeva.01@gmail.com>
2022-11-26 15:26:35 +0300

    fixed tasks

--- task_1/task1/index.js
+++ task_1/task1/index.js
@@ -1,7 +1,10 @@
 function sayHello(name) {
-    if (name == `Mark`) {
+    const SPECIAL_NAME = 'mark';
+
+    const isMark = name.toLowerCase() === SPECIAL_NAME;
+    if (isMark) {
         return `Hi, Mark!`
     } return `Hello, ${name}!`
 };
 
-console.log(sayHello(`Mark`));
+console.log(sayHello(`marK`));
--- task_1/task6/index.js
+++ task_1/task6/index.js
@@ -1,10 +1,16 @@
 // Написать функцию которая принимает в себя строчное значение состоящее из одного слова. В слове могут быть буквы разных регистров. Результатом выполнения данной функции должна быть строка, в которой первая буквы заглавная, а все последующие в нижнем регистре.Если в слове четное количество букв, то и последняя буква должна быть заглавной
 
 function getStringLine(stringText) {
-    if (stringText.length % 2 === 0) {
-        return stringText[0].toUpperCase() + stringText.slice(1, -1).toLowerCase() + stringText[stringText.length - 1].toUpperCase();
-    } return stringText[0].toUpperCase() + stringText.slice(1).toLowerCase();
+    const FirstCapitalizedLetter = stringText[0].toUpperCase();
+    const isCharsAmountEven = stringText.length % 2 === 0;
 
+    if (isCharsAmountEven) {
+        const LastCapitalizedLetter = stringText[ stringText.length - 1 ].toUpperCase();
+        const restWord = stringText.slice(1, -1).toLowerCase();
+
+        return `${ FirstCapitalizedLetter }${ restWord}${ LastCapitalizedLetter }`;
+    }
+    return `${FirstCapitalizedLetter}${stringText.slice(1).toLowerCase()}`
 }
 
 console.log(getStringLine('javaScrIpt'))
